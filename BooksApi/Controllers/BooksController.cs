7d7ac47b29c4d9f4f4c121b93b735cd2e5b013e9
gitName <guimvitor@gmail.com>
2022-03-23 21:33:55 -0300

    Web API with MongoDB books api

---
+++ BooksApi/Controllers/BooksController.cs
@@ -0,0 +1,81 @@
+﻿using BooksApi.Models;
+using BooksApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BooksApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookService _bookService;
+
+        public BooksController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Book>> Get() =>
+            _bookService.Get();
+
+        [HttpGet("{id:length(24)}", Name = "GetBook")]
+        public ActionResult<Book> Get(string id)
+        {
+            var book = _bookService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+
+        [HttpPost]
+        public ActionResult<Book> Create(Book book)
+        {
+            _bookService.Create(book);
+
+            return CreatedAtRoute("GetBook", new { id = book.Id.ToString() }, book);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Book bookIn)
+        {
+            var book = _bookService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _bookService.Update(id, bookIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var book = _bookService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _bookService.Remove(id);
+
+            return NoContent();
+        }
+    }
+}
+/*The preceding web API controller:
+
+Uses the BookService class to run CRUD operations.
+Contains action methods to support GET, POST, PUT, and DELETE HTTP requests.
+Calls CreatedAtRoute in the Create action method to return an HTTP 201 response. 
+Status code 201 is the standard response for an HTTP POST method that creates a new resource on the server. 
+CreatedAtRoute also adds a Location header to the response. The Location header specifies the URI of the newly created book.*/
---
+++ BooksApi/Models/Book.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+
+namespace BooksApi.Models
+{
+    public class Book
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        [JsonProperty("Name")]
+        public string BookName { get; set; }
+        /*The BookName property is annotated with the [BsonElement] attribute. 
+         * The attribute's value of Name represents the property name in the MongoDB collection.*/
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+
+        public string Author { get; set; }
+    }
+}
+
+/*In the preceding class, the Id property is:
+    Required for mapping the Common Language Runtime (CLR) object to the MongoDB collection.
+    Annotated with [BsonId] to make this property the document's primary key.
+    Annotated with [BsonRepresentation(BsonType.ObjectId)] to allow passing the parameter as type string instead of an ObjectId structure. 
+        Mongo handles the conversion from string to ObjectId.
+*/
